7e3e93f35ce315e1acbe30b6a19bdb6160ce30c0
Reza Askari <reza-askari@hotmail.com>
2020-08-21 21:53:46 +0430

    fixes to js

--- js/script.js
+++ js/script.js
@@ -9,27 +9,6 @@ var typed_name = new Typed('.name', {
             showCursor: false,
             strings: ["Full-stack web developer and linux enthusiast."],
             typeSpeed: 100,
-            onStringTyped: function () {
-                anime({
-                    targets: '.fa-github',
-                    opacity: 1,
-                    duration: 1000,
-                    complete: function() {
-                        anime({
-                            targets: '.fa-envelope-open-text',
-                            opacity: 1,
-                            duration: 1000,
-                            complete: function() {
-                                anime({
-                                    targets: '.fa-telegram',
-                                    opacity: 1,
-                                    duration: 1000
-                                });
-                            }
-                        });
-                    }
-                });
-            }
         });
     }
 });
