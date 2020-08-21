a9903087cdc79cfa83c17ec949c0437dc8fd4c42
Reza Askari <reza-askari@hotmail.com>
2020-08-21 22:20:49 +0430

    Animations enhanced

--- js/script.js
+++ js/script.js
@@ -1,14 +1,52 @@
 
-var typed_name = new Typed('.name', {
-    showCursor: false,
-    strings: ["Reza Askari"],
-    typeSpeed: 130,
-    onStringTyped: function () {
-        var typed_description = new Typed('.description', {
-            startDelay: 10,
-            showCursor: false,
-            strings: ["Full-stack web developer and linux enthusiast."],
-            typeSpeed: 100,
-        });
-    }
-});
+function showContactInfo() {
+    anime({
+        targets: 'section#contactInfo',
+        duration: 2500,
+        opacity: 1
+    });
+}
+
+function typeDescription() {
+    var typed_description = new Typed('.description', {
+        startDelay: 10,
+        showCursor: false,
+        strings: [
+            "Full-stack web developer and linux enthusiast."
+        ],
+        typeSpeed: 100,
+        onStringTyped: () => showContactInfo()
+    });
+}
+
+function typeName() {
+    var typed_name = new Typed('.name', {
+        showCursor: false,
+        strings: ["Reza Askari"],
+        typeSpeed: 130,
+        onStringTyped: () => typeDescription()
+    });
+}
+
+function showAvatar() {
+    anime({
+        delay: 1000,
+        targets: 'main.card img',
+        duration: 1000,
+        opacity: 1,
+        complete: () => typeName()
+    });
+}
+
+function fadeInMainCard() {
+    anime({
+        delay: 1000,
+        targets: 'main.card',
+        duration: 1500,
+        opacity: 1,
+        complete: () => showAvatar()
+    });
+}
+
+fadeInMainCard();
+
